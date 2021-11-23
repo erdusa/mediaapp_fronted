fd84ea6f16cced198a2ac766cda49439d064d1d2
Eudis Rene Duarte Sanguino <plataforma@crediservir.com>
2021-11-23 12:06:53 -0500

    subida inicial del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PacienteComponent } from './pages/paciente/paciente.component';
+import { MedicoComponent } from './pages/medico/medico.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PacienteEdicionComponent } from './pages/paciente/paciente-edicion/paciente-edicion.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
+import { MedicoDialogoComponent } from './pages/medico/medico-dialogo/medico-dialogo.component';
+import { ExamenComponent } from './pages/examen/examen.component';
+import { ExamenEdicionComponent } from './pages/examen/examen-edicion/examen-edicion.component';
+import { EspecialidadComponent } from './pages/especialidad/especialidad.component';
+import { EspecialidadEdicionComponent } from './pages/especialidad/especialidad-edicion/especialidad-edicion.component';
+import { ConsultaComponent } from './pages/consulta/consulta.component';
+import { ConsultaEspecialComponent } from './pages/consulta-especial/consulta-especial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PacienteComponent,
+    MedicoComponent,
+    PacienteEdicionComponent,
+    MedicoDialogoComponent,
+    ExamenComponent,
+    ExamenEdicionComponent,
+    EspecialidadComponent,
+    EspecialidadEdicionComponent,
+    ConsultaComponent,
+    ConsultaEspecialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MaterialModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
